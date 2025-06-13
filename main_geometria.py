d82f1e23b5ae19bd1e86561f684e4d4a935c9066
F3RREIRX <lucasgabrieldeabreuferreira2@gmail.com>
2025-06-13 16:19:10 -0300

    Create main_geometria.py

---
+++ main_geometria.py
@@ -0,0 +1,11 @@
+def calcular_area_retangulo(base, altura):
+   
+    return base * altura
+
+def calcular_area_triangulo(base, altura):
+   
+    return (base * altura) / 2
+
+def calcular_area_circulo(raio):
+   
+    return 3.14 * (raio**2)
